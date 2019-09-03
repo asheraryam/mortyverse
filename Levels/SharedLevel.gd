9312fcbb56cb4084b4b49b34f9440a3d7bdefa91
Kotob (Groovy Antoid) <kotob@groovyantoid.com>
2019-09-03 17:51:11 +0300

    Changed grass tileset and cleaned up level scenes.

--- Levels/SharedLevel.gd
+++ Levels/SharedLevel.gd
@@ -7,9 +7,9 @@ export(TileSet) var bushes_tiles
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if owner and owner.has_method("player_set_active"):
-		get_node("Bushes").tile_set = bushes_tiles
+		get_node("Tilemaps/Bushes").tile_set = bushes_tiles
 		if terrain_tiles:
-			get_node("Terrain").tile_set = terrain_tiles
+			get_node("Tilemaps/Terrain").tile_set = terrain_tiles
 		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
