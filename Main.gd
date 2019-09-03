031c1558fc1dbb210a776783ba293773eb2fdb71
asheraryam <asheraryam@gmail.com>
2019-09-03 21:15:12 +0300

    Fixed flipping issue by tween a rotation method and forcing the end result to round to 90.

--- Main.gd
+++ Main.gd
@@ -10,7 +10,7 @@ var world_rotations = [
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	game.main_node = self
-	switch_world_to(get_tree().get_nodes_in_group("start_world")[0])
+	switch_world_to(get_tree().get_nodes_in_group("start_world")[0], false)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -32,7 +32,7 @@ func go_to_next_world():
 #	$Center/Tween.stop_all()
 	var target_index = (game.current_world.INDEX + 1) % world_rotations.size()
 	
-	switch_world_to(get_world_by_id(target_index))
+	switch_world_to(get_world_by_id(target_index), true)
 #	var duration = 0.5
 #	$Center/Tween.interpolate_property($Center,"rotation", 
 #	$Center.rotation, 
@@ -45,11 +45,12 @@ func go_to_prev_world():
 	if game.current_world.INDEX == 0:
 		target_index = world_rotations.size()-1
 	
-	switch_world_to(get_world_by_id(target_index))
+	switch_world_to(get_world_by_id(target_index), false)
 
 var target_world
-func switch_world_to(_target_world):
+func switch_world_to(_target_world, forward):
 	if $Center/CenterTween.is_active():
+		print("Tween active, exiting")
 		return
 	target_world = _target_world
 	var duration = 0.5
@@ -60,14 +61,19 @@ func switch_world_to(_target_world):
 		duration = 0.001
 	game.bgm.fade_in(str(_target_world.INDEX))
 		
-	var current_degrees = $Center.rotation_degrees
+	var current_degrees = $Center.rotation.x
 #	var target_degrees = current_degrees
 	var target_degrees = world_rotations[_target_world.INDEX]
 #	target_degrees.x = deg2rad(world_rotations[_target_world.INDEX].x)
-#	if game.current_world.INDEX < _target_world.INDEX:
-#		target_degrees.x += deg2rad(90)
-#	else:
-#		target_degrees.x -= deg2rad(90)
+	if game.current_world:
+		if forward:
+			target_degrees.x = deg2rad(5.4545)
+		else:
+			target_degrees.x = - deg2rad(5.4545)
+	else:
+		target_degrees.x = 0
+		
+	var target_angle = target_degrees.x
 		
 #	var target_degrees = current_degrees 
 #	if game.current_world.INDEX < _target_world.INDEX or (
@@ -85,13 +91,28 @@ func switch_world_to(_target_world):
 	var players = get_tree().get_nodes_in_group("player")
 	for p in players:
 		p.update_facing()
+	
+	print("Attempting to start world rotation")
 
-	$Center/CenterTween.interpolate_property($Center,"rotation_degrees", 
-	current_degrees, 
-	target_degrees,0.5,Tween.TRANS_SINE,Tween.EASE_IN_OUT)
+	$Center/CenterTween.interpolate_method(self,"rotate_world_center_by", 
+	0, 
+	target_angle,0.5,Tween.TRANS_SINE,Tween.EASE_IN_OUT)
 	$Center/CenterTween.start()
 
+var sum_of_rotations = 0
+func rotate_world_center_by(radian_angle):
+	sum_of_rotations += radian_angle
+#	if rad2deg(sum_of_rotations) <=90:
+	$Center.rotate_x(radian_angle)
+#	else:
+#		sum_of_rotations -= radian_angle
+
 func _on_Center_tween_completed(object, key):
+	var needed = (deg2rad(90) * sign(sum_of_rotations)) - sum_of_rotations
+	print("Angle difference was " + str(needed))
+	$Center.rotate_x(needed)
+	print("Sum of rotations is : " + str(rad2deg(sum_of_rotations + needed)))
+	sum_of_rotations = 0
 	target_world.set_active()
 
 func pause_worlds():
