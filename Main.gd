095b57adeaac2ebee311837aeb0848d1275aa77f
asheraryam <asheraryam@gmail.com>
2019-09-03 21:52:46 +0300

    Pass index change to make later world selection easier to implement.

--- Main.gd
+++ Main.gd
@@ -10,7 +10,7 @@ var world_rotations = [
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	game.main_node = self
-	switch_world_to(get_tree().get_nodes_in_group("start_world")[0], false)
+	switch_world_to(get_tree().get_nodes_in_group("start_world")[0], 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -32,7 +32,7 @@ func go_to_next_world():
 #	$Center/Tween.stop_all()
 	var target_index = (game.current_world.INDEX + 1) % world_rotations.size()
 	
-	switch_world_to(get_world_by_id(target_index), true)
+	switch_world_to(get_world_by_id(target_index), 1)
 #	var duration = 0.5
 #	$Center/Tween.interpolate_property($Center,"rotation", 
 #	$Center.rotation, 
@@ -45,12 +45,12 @@ func go_to_prev_world():
 	if game.current_world.INDEX == 0:
 		target_index = world_rotations.size()-1
 	
-	switch_world_to(get_world_by_id(target_index), false)
+	switch_world_to(get_world_by_id(target_index), -1)
 
 var target_world
 var before_basis
 var after_basis
-func switch_world_to(_target_world, forward):
+func switch_world_to(_target_world, index_change):
 	if $Center/CenterTween.is_active():
 		print("Tween active, exiting")
 		return
@@ -66,10 +66,7 @@ func switch_world_to(_target_world, forward):
 	var current_degrees = $Center.rotation.x
 	var target_degrees = world_rotations[_target_world.INDEX]
 	if game.current_world:
-		if forward:
-			target_degrees.x = deg2rad(90)
-		else:
-			target_degrees.x = - deg2rad(90)
+		target_degrees.x = deg2rad(90) * index_change
 	else:
 		target_degrees.x = 0
 		
